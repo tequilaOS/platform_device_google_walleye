5f22b8f07762d5348d6fd4580052892d6faacdaa
Danny Lin <danny@kdrag0n.dev>
2020-12-17 14:09:01 -0800

    walleye: Remove redundant packages
    Dialer, Launcher3QuickStep, and netutils-wrapper-1.0 are already built
    in the system.
    
    WallpaperPicker should not be built as we have WallpaperPicker2 as part
    of ThemePicker.
    
    None of these packages belong in the device tree.
    
    Change-Id: Ia7c005caa09125490dbe8f071055e2059dcc4cb2

--- aosp_walleye.mk
+++ aosp_walleye.mk
@@ -29,10 +29,6 @@ $(call inherit-product, vendor/google/walleye/walleye-vendor.mk)
 $(call inherit-product-if-exists, vendor/google_devices/walleye/proprietary/device-vendor-walleye.mk)
 
 PRODUCT_PACKAGES += \
-    netutils-wrapper-1.0 \
-    Dialer \
-    Launcher3QuickStep \
-    WallpaperPicker \
     vndk_package
 
 PRODUCT_COPY_FILES += \
